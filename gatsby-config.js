46276b9a67ce2ae3eca9481105c0b3a944c1463d
danjoemybro <danjoestubbs@gmail.com>
2022-03-10 13:34:02 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
